c3a149010b03ea141feae939db0ad659818cc89e
ElisaSoleha <elisa.soleha1387@student.unri.ac.id>
2022-12-18 04:16:40 -0800

    menambahkan Aritmatika Dasar

---
+++ Aritmatika Dasar/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace aritmatikaDasar
+{
+     class Aritmatika{
+        static void Main( string[] args){
+            int tes = 7;
+            //Aritmatika penjumlahan
+            //tes = tes + 3;
+            //tes += 3;
+            //tes  ++; 
+
+            //aritmatika pengurangan
+            //tes = tes - 3 ;
+            //tes -= 3;
+            //tes --;
+
+            //Aritmatika perkalian
+            //tes = tes * 3;
+            //tes *= 3;
+
+            //Aritmatika pembagian
+            //tes = tes/ 3; karna menggunkan int maka output akan menjadi dua
+            //tes /= 3;
+
+            //int coba = tes % 3; akan membaca nilai yang tersisa
+
+        
+
+            Console.WriteLine(tes);
+
+
+            Console.ReadKey();
+        }
+     }
+}
