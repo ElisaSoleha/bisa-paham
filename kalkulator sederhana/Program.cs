60147f9a9b6f5272e7340d9ca33c9f27f9601b53
ElisaSoleha <elisa.soleha1387@student.unri.ac.id>
2022-12-18 00:45:51 -0800

    menambahkan kalkulator sederhana +-/x

---
+++ kalkulator sederhana/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace nama{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Membuat Kalkulator Sederhana");
+            Console.WriteLine("Anda Ingin menambah/mengkali/membagi/mengurangi");
+            string jawaban = Console.ReadLine();
+            if (jawaban == "menambah")
+            {
+                Console.WriteLine("Masukkan angka : ");
+                double nambah = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Masukkan angka : ");
+                double nambah2 = Convert.ToDouble(Console.ReadLine());
+                double tambah = nambah+nambah2;
+                Console.Write("Hasil: "+tambah);   
+            }
+            else if (jawaban == "mengkali")
+            {
+                
+                Console.WriteLine("Masukkan angka : ");
+                double nambah = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Masukkan angka : ");
+                double nambah2 = Convert.ToDouble(Console.ReadLine());
+                double tambah = nambah*nambah2;
+                Console.Write("Hasil: "+tambah);   
+            }
+            else if(jawaban == "membagi"){
+                 Console.WriteLine("Masukkan angka : ");
+                double nambah = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Masukkan angka : ");
+                double nambah2 = Convert.ToDouble(Console.ReadLine());
+                double tambah = nambah/nambah2;
+                Console.Write("Hasil: "+tambah);   
+            }
+            else if (jawaban == "mengurangi"){
+                 Console.WriteLine("Masukkan angka : ");
+                double nambah = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Masukkan angka : ");
+                double nambah2 = Convert.ToDouble(Console.ReadLine());
+                double tambah = nambah-nambah2;
+                Console.Write("Hasil: "+tambah);   
+            }
+            
+        }
+    }
+}
