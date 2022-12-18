02f00edbec02743396eb824487f83e229075f440
ElisaSoleha <elisa.soleha1387@student.unri.ac.id>
2022-12-18 00:41:19 -0800

    menambahkan bool,integer,char simple

---
+++ New folder (2)/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace bisaaa{
+    class Program{
+        static void Main(string[] args){
+          
+          int x; //declaration
+          x = 123; // initialization
+
+          int y = 321; // declaration + initialization
+
+          int z = x + y;
+
+          int age = 21; // wh0le integer
+
+          double height = 175.5; // desimal number
+          bool alive = false;
+          char symbol = '@';
+          string name = "bael";
+
+          Console.WriteLine("Hello " + name );
+          Console.WriteLine("Your age is: " +  age);
+          Console.WriteLine("Your heinght is: " +height+"cm");
+          Console.WriteLine("Are you alive?" +alive);
+          Console.WriteLine("Ypur simbol is : " +symbol);
+
+          string userName = symbol + name ;
+
+          Console.WriteLine("Your user name is : " +userName  );
+
+
+        
+
+            Console.ReadKey();
+        }
+    }
+}
