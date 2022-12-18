af60cf4a4e520eacc3ca9b86790cfab5b4aff1ed
ElisaSoleha <elisa.soleha1387@student.unri.ac.id>
2022-12-18 01:26:31 -0800

    tak paham

---
+++ New folder (2)/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace konstan
+{
+    class Program
+    {
+        static void Main(string[] args){
+         //konstanta = nilai kebal yang diketahui pada waktu kompilasi
+         // dan tidak berubah selama masa hidup program
+
+
+         //example
+         double pi = 3.1415;
+         pi = 230;
+
+        
+
+         Console.WriteLine(pi);
+
+         // if use const
+
+         const double pie = 3.1415;
+         
+         Console.WriteLine(pie);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
