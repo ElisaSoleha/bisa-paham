2367bed91950e1767811803acd29ae5beaee04ba
ElisaSoleha <elisa.soleha1387@student.unri.ac.id>
2022-12-18 03:33:52 -0800

    menambahkan input dasar c#

---
+++ input dasar/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace inputuser
+{
+    class User
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Siapa Nama Kamu ?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Umur kamu berapa?");
+            int umur = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Hello "+ name);
+            Console.WriteLine($"Kamu berumur {umur} tahun");
+
+            Console.ReadKey();
+        }
+    }
+}
