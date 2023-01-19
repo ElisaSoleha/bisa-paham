22d7a8c9cbd716e34f2efce3ee43af08fe6b0e08
ElisaSoleha <elisa.soleha1387@student.unri.ac.id>
2023-01-19 06:44:46 -0800

    menambahkan contineu dan break

---
+++ Aritmatika Dasar/keren/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+namespace mana
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int b= 0;
+            while(b<10)
+            {
+                
+                if (b==6)
+                {
+                    b++;
+                  continue;
+               
+                }
+                Console.WriteLine(b);
+                b++;
+            } 
+            int c=0;
+            while(c<20){
+                Console.WriteLine(c);
+                c++;
+                if(c==15)
+                {
+                    break;
+                }
+            }
+        
+            for (int i = 0; i < 6; i++)
+            {
+
+                if(i==4)
+                {
+                    break;
+                }
+                Console.WriteLine(i);
+                
+            }
+            for (int i = 0; i < 7; i++)
+            {
+                if (i==5)
+                {
+                    continue;
+                }
+                Console.WriteLine(i);
+            }
+        } 
+    }
+}
