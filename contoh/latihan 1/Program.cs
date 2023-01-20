a3caca94ab91bf5d0a5431fe27e6a1071716cf03
ElisaSoleha <elisa.soleha1387@student.unri.ac.id>
2023-01-20 03:31:47 -0800

    menambah array

---
+++ contoh/latihan 1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace latihan1
+{
+    class dasar
+    {
+        static void Main(string[]args)
+        {
+            // string[] makanan = {"sate", "lontong", "bakso"};
+            // string[] minuman = {"Teh es", "susu", "Air putih"};
+            // string[] cemilan = {"keripik hijau", "ring-ring" , "potato"};
+
+
+            string [,] nama = {{"sate" ,"lontong" ,"bakso"},
+            {"Teh es", "susu", "Air putih"},
+            {"keripik hijau", "ring-ring" , "potato"}
+            };
+
+            nama[1,2] = "Kopi";
+            nama[2,2]= "air hangat";
+
+            // foreach (string kamu in nama){
+
+            // Console.WriteLine (kamu);
+            // }
+
+            for (int i = 0; i < nama.GetLength (0);  i++){
+                for (int m = 0; m < nama.GetLength(1); m++){
+                    Console.Write(nama[i, m]+" ");
+                }
+                Console.WriteLine();
+            }
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
