3f836e5f9dde506a0c63c44a637a487b8c55df2a
ElisaSoleha <elisa.soleha1387@student.unri.ac.id>
2023-01-20 07:04:20 -0800

    menambahkan mandiri static class

---
+++ Latihan2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace latihan2classes
+{
+    class dasar
+    {
+        static void Main(string[]args)
+        {
+            // Kumpulan kode terkait.
+            // dapat digunakan warna biru untuk membuat objek (OOP)
+
+            nama.Tes();
+            nama.satu ();
+
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ Latihan2/latihan2.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace latihan2classes
+{
+    static class nama
+    {
+     public static void Tes(){
+        Console.WriteLine("hallo apakah ini berfungsi");
+     }
+     public static void satu(){
+        Console.WriteLine("Jika berfungsi Ketikkan sesuatu");
+        string dua = Console.ReadLine();
+     }
+        
+    }
+}
